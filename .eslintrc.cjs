e5616973464d82465ab7875cf7a36a93abe10005
hotequil <hotequil@protonmail.com>
2023-11-06 21:14:14 -0300

    chore: config Prettier, EditorConfig and ESLint

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -5,14 +5,19 @@ module.exports = {
         'eslint:recommended',
         'plugin:@typescript-eslint/recommended',
         'plugin:react-hooks/recommended',
+        'plugin:prettier/recommended',
+        'prettier',
     ],
     ignorePatterns: ['dist', '.eslintrc.cjs'],
     parser: '@typescript-eslint/parser',
-    plugins: ['react-refresh'],
+    plugins: ['react-refresh', 'prettier'],
     rules: {
-        'react-refresh/only-export-components': [
+        'prettier/prettier': [
             'warn',
-            { allowConstantExport: true },
+            {
+                endOfLine: 'auto',
+                singleQuote: true,
+            },
         ],
     },
 }
