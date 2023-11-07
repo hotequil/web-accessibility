30015e93b41d153e8ea8803701350bd6b49da644
hotequil <hotequil@protonmail.com>
2023-11-06 21:42:13 -0300

    feat: added routes and init NotUseCapitalizedText component

---
+++ src/pages/NotUseCapitalizedText.tsx
@@ -0,0 +1,4 @@
+import React from 'react'
+import { Page } from '../layouts/Page'
+
+export const NotUseCapitalizedText: React.FC = () => <Page wrongChildren={'test1'} rightChildren={'test2'} />
---
+++ src/routes/index.ts
@@ -0,0 +1,7 @@
+export default {
+    notUseCapitalizedText: '/not-use-capitalized-text',
+    imageWithAlternativeText: '/image-with-alternative-text',
+    notUseClickHere: '/not-use-click-here',
+    linkAsButtonAndButtonAsLink: '/link-as-button-and-button-as-link',
+    useRadioNotButton: '/use-radio-not-button',
+}
