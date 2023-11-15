571a0ee60b68e86a28d83d80d808589d8e6d9644
hotequil <hotequil@protonmail.com>
2023-11-14 21:13:27 -0300

    feat: done code examples of alternative text

--- src/pages/ImageWithAlternativeText.tsx
+++ src/pages/ImageWithAlternativeText.tsx
@@ -4,6 +4,9 @@ import mazzaropiSmoking from '/mazzaropi-smoking.jpg'
 import movieOJecaEAFreiraByMazzaropi from '/movie-o-jeca-e-a-freira-by-mazzaropi.jpg'
 import styled from 'styled-components'
 import { transparentize } from 'polished'
+import imageWithAlternativeTextFirstWrong from '/code-examples/image-with-alternative-text-first-wrong.png'
+import imageWithAlternativeTextFirstRight from '/code-examples/image-with-alternative-text-first-right.png'
+import { CodeExamples } from '../components/CodeExamples'
 
 const Grid = styled.div`
     display: grid;
@@ -26,22 +29,44 @@ export const ImageWithAlternativeText: React.FC = () => {
     return (
         <Page
             wrongChildren={
-                <Grid>
-                    <Image src={mazzaropiSmoking} />
-                    <Image src={movieOJecaEAFreiraByMazzaropi} />
-                </Grid>
+                <>
+                    <Grid>
+                        <Image src={mazzaropiSmoking} />
+                        <Image src={movieOJecaEAFreiraByMazzaropi} />
+                    </Grid>
+
+                    <CodeExamples
+                        images={[
+                            {
+                                src: imageWithAlternativeTextFirstWrong,
+                                alt: 'Images without alt attribute for alternative description',
+                            },
+                        ]}
+                    />
+                </>
             }
             rightChildren={
-                <Grid>
-                    <Image
-                        src={mazzaropiSmoking}
-                        alt="Black and white photo of Mazzaropi's face smoking a pipe, looking to the left suspiciously"
+                <>
+                    <Grid>
+                        <Image
+                            src={mazzaropiSmoking}
+                            alt="Black and white photo of Mazzaropi's face smoking a pipe, looking to the left suspiciously"
+                        />
+                        <Image
+                            src={movieOJecaEAFreiraByMazzaropi}
+                            alt="The movie poster 'O Jeca e a Freira' of Mazzaropi, it shows him and his wife looking ahead. He's holding a pipe and wearing a hat, and there's a semi-transparent image of a nun in the sky"
+                        />
+                    </Grid>
+
+                    <CodeExamples
+                        images={[
+                            {
+                                src: imageWithAlternativeTextFirstRight,
+                                alt: 'Images using the alt attribute correctly to describe them',
+                            },
+                        ]}
                     />
-                    <Image
-                        src={movieOJecaEAFreiraByMazzaropi}
-                        alt="The movie poster 'O Jeca e a Freira' of Mazzaropi, it shows him and his wife looking ahead. He's holding a pipe and wearing a hat, and there's a semi-transparent image of a nun in the sky"
-                    />
-                </Grid>
+                </>
             }
         />
     )
